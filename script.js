c994a59ebbdbc908392f811985ff30c874dcf906
Jelena <jelenakrstic164@gmail.com>
2019-03-16 18:06:05 +0100

    Changed a preloader

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-
+$(window).on('load', function() { // makes sure the whole site is loaded 
+  $('#status').fadeOut(); // will first fade out the loading animation 
+  $('.pageloader').delay(250).fadeOut('slow'); // will fade out the white DIV that covers the website. 
+  $('body').delay(250).css({'overflow':'visible'});
+})
 
 $(document).ready(function() {
 
