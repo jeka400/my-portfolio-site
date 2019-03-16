590e8ea2edbb0a3fe7303a8e796fd0a1689018b1
Jelena <jelenakrstic164@gmail.com>
2019-03-16 18:36:30 +0100

    Changed a preloader

--- script.js
+++ script.js
@@ -54,6 +54,23 @@ $(document).ready(function() {
         
     });
 
+    // ==========================================     Intro - Text effect       ======================================================
+    $('.sectionOneAnimation').each(function(){
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline().add({
+        targets: '.sectionOneAnimation .letter',
+        scale: [4,1],
+        opacity: [0,1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: function(el, i) {
+            return 70*i;
+        }
+    });
+
     // ================================================      Popup       ===========================================================
     // =======================  Myko Minions
 
@@ -354,24 +371,6 @@ $(document).ready(function() {
           return withinViewport;
         
     }());
-
-
-    // Text effect
-    $('.sectionOneAnimation').each(function(){
-        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-    });
-
-    anime.timeline().add({
-        targets: '.sectionOneAnimation .letter',
-        scale: [4,1],
-        opacity: [0,1],
-        translateZ: 0,
-        easing: "easeOutExpo",
-        duration: 950,
-        delay: function(el, i) {
-            return 70*i;
-        }
-    });
 
 })
 
